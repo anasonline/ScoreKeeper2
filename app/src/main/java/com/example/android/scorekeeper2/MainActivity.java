2896a851b88a45ae09fa3ad2db9129baa899a69b
Anas <anas.marrawi@gmail.com>
2017-03-05 10:30:42 +0100

    Add functionality for calculating goals and fouls for both teams

--- app/src/main/java/com/example/android/scorekeeper2/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper2/MainActivity.java
@@ -1,13 +1,121 @@
 package com.example.android.scorekeeper2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int goalsTeamA = 0;
+    int yellowCardsTeamA = 0;
+    int redCardsTeamA = 0;
+
+    int goalsTeamB = 0;
+    int yellowCardsTeamB = 0;
+    int redCardsTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void addGoalTeamA(View v) {
+
+        goalsTeamA = goalsTeamA + 1;
+        displayGoalsForTeamA(goalsTeamA);
+
+    }
+
+    public void addYellowCardTeamA(View v) {
+
+        yellowCardsTeamA = yellowCardsTeamA + 1;
+        displayYellowCardsForTeamA(yellowCardsTeamA);
+
+    }
+
+    public void addRedCardTeamA(View v) {
+
+        redCardsTeamA = redCardsTeamA + 1;
+        displayRedCardsForTeamA(redCardsTeamA);
+
+    }
+
+
+    public void addGoalTeamB(View v) {
+
+        goalsTeamB = goalsTeamB + 1;
+        displayGoalsForTeamB(goalsTeamB);
+
+    }
+
+    public void addYellowCardTeamB(View v) {
+
+        yellowCardsTeamB = yellowCardsTeamB + 1;
+        displayYellowCardsForTeamB(yellowCardsTeamB);
+
+    }
+
+    public void addRedCardTeamB(View v) {
+
+        redCardsTeamB = redCardsTeamB + 1;
+        displayRedCardsForTeamB(redCardsTeamB);
+
+    }
+
+
+    /**
+     * Displays the given number of Goals for Team A.
+     */
+    public void displayGoalsForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_goals);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given number of Yellow Cards for Team A.
+     */
+
+    public void displayYellowCardsForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_yellow_cards);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given number of Yellow Cards for Team A.
+     */
+
+    public void displayRedCardsForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_red_cards);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+    /**
+     * Displays the given number of Goals for Team B.
+     */
+    public void displayGoalsForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_goals);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given number of Yellow Cards for Team B.
+     */
+
+    public void displayYellowCardsForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_yellow_cards);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given number of Yellow Cards for Team A.
+     */
+
+    public void displayRedCardsForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_red_cards);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
